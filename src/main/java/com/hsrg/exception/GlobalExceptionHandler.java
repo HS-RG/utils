933f72ecd7136db45f96c7f200e214bc36951313
wxl <1715142548@qq.com>
2023-05-24 23:28:14 +0800

    单独处理自定义异常类MyFeignException

--- src/main/java/com/hsrg/exception/GlobalExceptionHandler.java
+++ src/main/java/com/hsrg/exception/GlobalExceptionHandler.java
@@ -9,13 +9,16 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(MyFeignException.class)//捕获来自feign的异常
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)//返回http500状态，代表有异常发生
+    public Result ex1(MyFeignException ex){
+        ex.printStackTrace();
+        return Result.error(ex.getMsg()+"……来自被调用服务的异常");
+    }
     @ExceptionHandler(Exception.class)//捕获所有异常
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)//返回http500状态，代表有异常发生
     public Result ex(Exception ex){
         ex.printStackTrace();
-        if(ex.getClass().equals(MyException.class)){
-            return Result.error(((MyException) ex).getMsg());
-        }
         return Result.error("对不起，操作失败");
     }
 
