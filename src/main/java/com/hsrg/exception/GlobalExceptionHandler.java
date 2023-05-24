cb05bdd570784438970e9ab417e069037611f049
wxl <1715142548@qq.com>
2023-05-24 22:34:22 +0800

    异常发生后响应状态设置为500

--- src/main/java/com/hsrg/exception/GlobalExceptionHandler.java
+++ src/main/java/com/hsrg/exception/GlobalExceptionHandler.java
@@ -1,13 +1,16 @@
 package com.hsrg.exception;
 
 import com.hsrg.pojo.Result;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)//捕获所有异常
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)//返回http500状态，代表有异常发生
     public Result ex(Exception ex){
         ex.printStackTrace();
         return Result.error("对不起，操作失败");
