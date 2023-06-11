4e1132e67ed0a98b5efd3d8c40e902bfca76a7a8
wxl <1715142548@qq.com>
2023-06-11 21:57:51 +0800

    添加获取资源所有评论返回的内容如头像url之类

--- src/main/java/com/hsrg/pojo/Comment.java
+++ src/main/java/com/hsrg/pojo/Comment.java
@@ -13,8 +13,11 @@ public class Comment {
     private Long commentId;
     private Long parentId;
     private Integer type;
+    private String parentNickname;
     private Long replyTo;
     private Long authorId;
+    private String authorNickname;
+    private String authorImageUrl;
     private String context;
     private LocalDateTime updateTime;
     private LocalDateTime createTime;
